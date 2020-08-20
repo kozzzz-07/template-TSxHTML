80fee1cbb3c97a07288068fcfd6fc3ed02a1c632
kozzzz-07 <kozzzz-07@noreply>
2020-08-21 02:06:15 +0900

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# TypeScript×HTML のテンプレート
---
+++ webpack.config.ts
@@ -0,0 +1,43 @@
+import * as path from "path";
+import * as webpack from "webpack";
+import HtmlWebPackPlugin from "html-webpack-plugin";
+
+const module: webpack.Module = {
+  rules: [
+    {
+      test: /\.ts$/,
+      loader: "ts-loader",
+    },
+    {
+      test: /\.html$/,
+      loader: "html-loader",
+    },
+  ],
+};
+
+const resolve: webpack.Resolve = {
+  extensions: [".ts", ".js"],
+};
+
+const config: webpack.Configuration = {
+  // mode: "development",
+  entry: "./src/index.ts",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html",
+    }),
+  ],
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: "./dist",
+  },
+  module,
+  resolve,
+};
+
+export default config;
